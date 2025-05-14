1b47227ee66372a1ee08cea1b22538c9d0f40a44
Vitoremanuel90 <vitoremanuel18872@gmail.com>
2025-05-14 20:08:45 -0300

    Add files via upload

---
+++ kmlt.py
@@ -0,0 +1,7 @@
+print (" 🚗🚕🚙🚐Seja Bem vindo 🚗🚕🚙🚐 ")
+print ( "Esse programa soma quantos km você fez por litro  ;) ")
+km =float(input("Digite a quantidade de Km: "))
+lt = float(input("Digite quantos litros você gastou: "))
+quantidade= km/lt
+print (f"O comsumo do seu carro por litro percorridos e de ‼️ : {quantidade} ")
+print (" 😃😃 Obrigado por usar o somador de KM é volte sempre! 😃😃 ")
